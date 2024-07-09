1e00d8a6a97da6527b22e22d35dcb156a844bee8
Giorgi Kravchenko <138400870+DonKravche@users.noreply.github.com>
2024-07-10 00:16:18 +0400

    done HomeWork N6

--- HomeWork N6/HomeWork N6.py
+++ HomeWork N6/HomeWork N6.py
@@ -1,32 +1,32 @@
-# # 1
-# def anagram(str1, str2):
-#     if len(str1) != len(str2):
-#         return f'{str1} and {str2} are not anagrams'
-#     if sorted(str1) == sorted(str2):
-#         return f'{str1} and {str2} are anagrams'
-#     else:
-#         return f'{str1} and {str2} are not anagrams'
-#
-#
-# print(anagram('sword', 'words'))
-# print(anagram('listen', 'silent'))
-#
-#
-# # 2
-#
-# def count_symbol(string, symbol):
-#     count = 0
-#     for char in string:
-#         if char == symbol:
-#             count += 1
-#     return count
-#
-#
-# input_string = input('Enter a string: ')
-# input_symbol = input('Enter a symbol to count: ')
-#
-# result = count_symbol(input_string, input_symbol)
-# print(f"The symbol '{input_symbol}' appears {result} times in the string.")
+# 1
+def anagram(str1, str2):
+    if len(str1) != len(str2):
+        return f'{str1} and {str2} are not anagrams'
+    if sorted(str1) == sorted(str2):
+        return f'{str1} and {str2} are anagrams'
+    else:
+        return f'{str1} and {str2} are not anagrams'
+
+
+print(anagram('sword', 'words'))
+print(anagram('listen', 'silent'))
+
+
+# 2
+
+def count_symbol(string, symbol):
+    count = 0
+    for char in string:
+        if char == symbol:
+            count += 1
+    return count
+
+
+input_string = input('Enter a string: ')
+input_symbol = input('Enter a symbol to count: ')
+
+result = count_symbol(input_string, input_symbol)
+print(f"The symbol '{input_symbol}' appears {result} times in the string.")
 
 
 # 3
